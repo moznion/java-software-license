b624b56d903e337c6bef2957cafef1b75eaf3501
moznion <moznion@gmail.com>
2015-02-16 23:19:34 +0900

    Initial commit

---
+++ src/main/java/net/moznion/software/license/App.java
@@ -0,0 +1,12 @@
+package net.moznion.software.license;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
